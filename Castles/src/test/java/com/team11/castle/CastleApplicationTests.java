cb1473a87b873e18808eb5ee09a76c17dc577c4c
SL-207 <sl207020@gmail.com>
2025-03-25 22:51:36 +0000

    Commit SpringBoot base code

---
+++ Castles/src/test/java/com/team11/castle/CastleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team11.castle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CastleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
