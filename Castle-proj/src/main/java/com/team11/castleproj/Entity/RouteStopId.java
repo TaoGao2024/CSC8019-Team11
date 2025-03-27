f8bd3ce02e589b6618398c1df1d5711b50cb1060
SL-207 <sl207020@gmail.com>
2025-03-27 17:05:23 +0000

    Configure database schema

--- Castle-proj/src/main/java/com/team11/castleproj/Entity/RouteStopId.java
+++ Castle-proj/src/main/java/com/team11/castleproj/Entity/RouteStopId.java
@@ -2,6 +2,7 @@ package com.team11.castleproj.Entity;
 
 import jakarta.persistence.Embeddable;
 import java.io.Serializable;
+import java.util.Objects;
 
 @Embeddable
 public class RouteStopId implements Serializable {
@@ -13,6 +14,19 @@ public class RouteStopId implements Serializable {
     public RouteStopId(int routeId, int stopId) {
         this.routeId = routeId;
         this.stopId = stopId;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        RouteStopId that = (RouteStopId) o;
+        return routeId == that.routeId && stopId == that.stopId;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(routeId, stopId);
     }
 
 }
