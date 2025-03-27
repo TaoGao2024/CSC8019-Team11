d318b9e3f7558bbdca3ed482b9ddc55f7d64f39e
TaoGao2024 <hugogaotao@gmail.com>
2025-03-27 16:32:31 +0000

    database create successfully

--- Castle-proj/src/main/java/com/team11/castleproj/Entity/CastleInfo.java
+++ Castle-proj/src/main/java/com/team11/castleproj/Entity/CastleInfo.java
@@ -3,10 +3,16 @@ package com.team11.castleproj.Entity;
 //import jakarta.persistence.Entity;
 //import jakarta.persistence.Id;
 //import jakarta.persistence.Table;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Data;
 
 @Data
+@Entity
+@Table(name="CastleInfo")
 public class CastleInfo {
+    @Id
     private int castleId;
     private String castleName;
     private String castleLocation;
