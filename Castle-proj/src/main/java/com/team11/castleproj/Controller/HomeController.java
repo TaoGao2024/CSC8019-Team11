a80d689f9aa2c9ce242e94dc8836be5be75b801a
TaoGao2024 <hugogaotao@gmail.com>
2025-04-30 23:42:58 +0100

    connect frontend with backend

--- Castle-proj/src/main/java/com/team11/castleproj/Controller/HomeController.java
+++ Castle-proj/src/main/java/com/team11/castleproj/Controller/HomeController.java
@@ -74,11 +74,6 @@ public class HomeController {
                                  Model model) {
         List<CastleInfo> castleInfoList = castleInfoRepository.findByName(castleName);
         
-        if (castleInfoList.isEmpty()) {
-            model.addAttribute("error", "找不到城堡信息: " + castleName);
-            return "error"; // 需要创建error.html页面
-        }
-        
         String castleId = castleInfoList.get(0).getCastleId();
         double entryFee = castleInfoList.get(0).getEntryFee();
 
